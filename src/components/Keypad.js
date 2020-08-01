f66db17a65c23b5c3c6f9b69b7994c5406682663
kierra <51520037+kierrakay@users.noreply.github.com>
2020-08-01 13:06:45 -0400

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,23 @@
 // Code Keypad Component Here
+import React, { Component } from 'react'
+
+class Keypad extends Component {
+    
+
+    handleInputPassword = () => {
+        console.log('Entering password...')
+    }
+
+    render () {
+        return (
+            <input type="password"
+                onKeyUp={this.handleInputPassword} />
+        )
+
+    }
+}
+
+export default Keypad
+
+//handleInputPassword is an event handler that listens for the keyUp event
+// onKeyUp is the event. anything with "on" infront of it is an event
