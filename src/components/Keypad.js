73b481a4a7c88f7a1e921d11563361356ccd1b5d
Alouiscious Media <info@reneesoul.com>
2020-08-28 05:29:07 -0700

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,25 @@
-// Code Keypad Component Here
+import React, { Component } from 'react';
+
+
+export default class Keypad  extends Component {
+  
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     password: this.props.userPassword
+  //   }
+  // }
+
+  enterPassword = () => {
+    console.log("Entering password...")
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="password" onKeyUp={this.enterPassword} placeholder="Enter Password">
+        </input>
+      </div>
+    )
+  }
+}
